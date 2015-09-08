bcf9fb01cdf4470b77921e547e74b4ea8a8541fd
Michael Persson <michael.ake.persson@gmail.com>
2015-09-08 12:05:54 +0200

    Updated example

--- example/main.go
+++ example/main.go
@@ -1,42 +1,61 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
 
-	"github.com/coreos/go-etcd/etcd"
+	etcd "github.com/coreos/go-etcd/etcd"
 	"github.com/mickep76/etcdmap"
 )
 
 type User struct {
-	Name      string `json:"user"`
+	Name      string `json:"username"`
 	FirstName string `json:"first_name"`
 	LastName  string `json:"last_name"`
 }
 
+type Group struct {
+	Name  string `json:"groupname"`
+	users []User `json:"users"`
+}
+
 func main() {
+	verbose := flag.Bool("verbose", false, "Verbose")
+	node := flag.String("node", "localhost", "Etcd node")
+	port := flag.String("port", "5001", "Etcd port")
+	flag.Parse()
+
 	u := User{
 		Name:      "jdoe",
 		FirstName: "John",
 		LastName:  "Doe",
 	}
 
-	node := []string{fmt.Sprintf("http://%v:%v", "192.168.99.100", "5001")}
-	client := etcd.NewClient(node)
+	// Connect to Etcd.
+	dbo := []string{fmt.Sprintf("http://%v:%v", *node, *port)}
+	if *verbose {
+		log.Printf("Connecting to: %s", node)
+	}
+	client := etcd.NewClient(dbo)
 
-	err := etcdmap.CreateStruct(client, "/user/jdoe", &u)
+	// Create directory structure based on struct.
+	err := etcdmap.CreateStruct(client, "/example", &u)
 	if err != nil {
-		fmt.Println(err.Error)
+		log.Fatal(err.Error)
 	}
 
-	res, err := client.Get("/test", true, true)
+	// Get directory structure from Etcd.
+	res, err := client.Get("/example", true, true)
 	if err != nil {
-		fmt.Println(err.Error())
+		log.Fatal(err.Error())
 	}
 
+	// Convert Etcd node to struct.
 	r := User{}
 	err2 := etcdmap.Struct(res.Node, &r)
 	if err2 != nil {
-		fmt.Println(err.Error)
+		log.Fatal(err.Error)
 	}
 
 	fmt.Println(r)
